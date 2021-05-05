cd51e453b1568da40840bac3142bc84cfeb9aeb8
krisMONSTER <62694355+krisMONSTER@users.noreply.github.com>
2021-05-05 22:23:11 +0200

    Add project files.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MechanicApp {
+    public class BundleConfig {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/External/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/External/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/External/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/External/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/External/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MechanicApp.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            if (Session["LoggedIn"] != null) {
+                return View("~/Views/Home/Login.cshtml");
+            } else {
+                return View("~/Views/Home/Login.cshtml");
+            }
+        }
+
+        public ActionResult Register() {
+            return View();
+        }
+
+        /*public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }*/
+    }
+}
